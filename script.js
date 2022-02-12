9d7e9c39a4c877c20234bcf5d35a34c402c9cdbe
Shubhran <shubhran.sharma@gmail.com>
2022-02-12 19:18:15 +0530

    Button Color Changer

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let i = 1;
 let H1 = document.getElementById("H1");
 function buttonClick() {
     if (i==0) {
-        H1.color = red;
+        H1.style.color = red;
         i++;
     } else {
-        H1.color = blue;
+        H1.style.color = blue;
         i--;
     } 
 }
