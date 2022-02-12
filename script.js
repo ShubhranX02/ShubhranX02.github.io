44a233dad5805cbbfe34839df4e12c6ab6a50e3e
Shubhran <shubhran.sharma@gmail.com>
2022-02-12 19:48:37 +0530

    Button Color-Changer Final

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let i = 1;
-let H1 = document.getElementById("H1");
 function buttonClick() {
+    let H1 = document.getElementById("H1");
     if (i==0) {
-        H1.style.color = red;
+        H1.innerText = "My Name Is Shubhran!";
         i++;
     } else {
-        H1.style.color = blue;
+        H1.innerText = "How Are You?";
         i--;
     } 
 }
